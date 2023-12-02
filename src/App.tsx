f7a8d35226fc7119777dccb650b024b17cebabeb
Kshitij Verma <kshitijv250@gmail.com>
2023-12-03 01:01:18 +0530

    changed style of whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./index.css";
-import sendIcon from "./assets/send.png";
 import React from "react";
 
 function App() {
@@ -31,14 +30,14 @@ function App() {
 
   return (
     <>
-      <div className="bg-slate-800 w-screen h-screen flex flex-col items-center pt-8">
-        <h1 className="text-sky-400 py-4 text-6xl font-bold">
+      <div className="bg-[url('/hindi_back.png')] bg-cover w-screen h-screen flex flex-col items-center pt-8">
+        <h1 className="text-rose-400 py-4 text-6xl font-bold">
           हिंदी प्रश्न उत्तर
         </h1>
         <form className="flex flex-col" action="/submit">
-          <h1 className="text-3xl p-2 text-white font-bold">संदर्भ</h1>
+          <h1 className="text-3xl p-2 text-rose-200 font-bold">संदर्भ</h1>
           <textarea
-            className="rounded-md text-2xl bg-slate-700 text-white p-4 mb-4 focus:outline-none focus:ring-4 focus:ring-blue-600 focus:border-transparent"
+            className="rounded-md text-2xl bg-rose-300/50 backdrop-blur-md text-rose-200 p-4 mb-4 focus:outline-none focus:ring-4 focus:ring-rose-600 focus:border-transparent placeholder:text-rose-200"
             placeholder="कृपया संदर्भ भरें"
             name="context"
             value={context}
@@ -47,10 +46,10 @@ function App() {
             cols={60}
             rows={8}
           ></textarea>
-          <h1 className="p-2 text-3xl text-white font-bold">प्रश्न</h1>
+          <h1 className="p-2 text-3xl text-rose-200 font-bold">प्रश्न</h1>
           <div className="flex">
             <input
-              className="rounded-md text-2xl bg-slate-700 text-white p-4 w-[54rem] focus:outline-none focus:ring-4 focus:ring-blue-600 focus:border-transparent"
+              className="rounded-md text-2xl bg-rose-300/50 backdrop-blur-md text-rose-100 p-4 w-[54rem] focus:outline-none focus:ring-4 focus:ring-rose-600 focus:border-transparent placeholder:text-rose-200"
               name="question"
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
@@ -59,18 +58,18 @@ function App() {
             />
 
             <button
-              className="rounded-md bg-sky-400 p-3 ml-4"
+              className="rounded-md bg-rose-400 py-3 px-4 ml-4 shadow-md active:bg-rose-500 active:shadow-none"
               type="submit"
               onClick={onSubmithandler}
             >
-              <img className="h-8 w-8" src={sendIcon} alt="" />
+              <img className="h-8 w-8" src={"/send.svg"} alt="" />
             </button>
           </div>
           {answer && (
             <div className="mt-4">
-              <h1 className="text-3xl p-2 text-white font-bold">उत्तर</h1>
-              <div className="bg-slate-700 p-4 mt-2 rounded-md">
-                <p className="text-white text-2xl">{answer}</p>
+              <h1 className="text-3xl p-2 text-rose-200 font-bold">उत्तर</h1>
+              <div className="bg-rose-300/50 backdrop-blur-md p-4 mt-2 rounded-md">
+                <p className="text-rose-100 text-2xl">{answer}</p>
               </div>
             </div>
           )}
